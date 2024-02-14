bd1f98352b8b42aab292fb8609f555282ae1e6e5
Mert Akman <mertakman2012@gmail.com>
2024-02-14 06:38:54 +0200

    Fix:make turn restricted djikstra accept only source and target instead of 3 way restrictions

--- graph.go
+++ graph.go
@@ -12,7 +12,7 @@ import (
 // shortcuts Found and stored shortcuts based on contraction hierarchies
 type Graph struct {
 	shortcuts    []map[int64]*ShortcutPath
-	restrictions map[int64]map[int64]map[int64]bool
+	restrictions map[int64]map[int64]bool
 	mapping      map[int64]int64
 
 	Vertices     []Vertex
@@ -38,7 +38,7 @@ func NewGraph() *Graph {
 		Vertices:     make([]Vertex, 0),
 		edgesNum:     0,
 		shortcutsNum: 0,
-		restrictions: make(map[int64]map[int64]map[int64]bool),
+		restrictions: make(map[int64]map[int64]bool),
 		frozen:       false,
 		verbose:      false,
 	}
@@ -206,28 +206,23 @@ func (graph *Graph) GetEdgesNum() int64 {
 // from User's definied ID of source vertex
 // via User's definied ID of prohibited vertex (between source and target)
 // to User's definied ID of target vertex
-func (graph *Graph) AddTurnRestriction(from, via, to int64) error {
+func (graph *Graph) AddTurnRestriction(from, to int64) error {
 	if graph.frozen {
 		return ErrGraphIsFrozen
 	}
 
 	from = graph.mapping[from]
-	via = graph.mapping[via]
 	to = graph.mapping[to]
 
 	if graph.restrictions == nil {
-		graph.restrictions = make(map[int64]map[int64]map[int64]bool)
+		graph.restrictions = make(map[int64]map[int64]bool)
 	}
 
 	if _, ok := graph.restrictions[from]; !ok {
-		graph.restrictions[from] = make(map[int64]map[int64]bool)
-		toMap, ok := graph.restrictions[from][via]
-		if !ok {
-			toMap = make(map[int64]bool)
-			graph.restrictions[from][via] = toMap
-		}
-		graph.restrictions[from][via][to] = true
+		graph.restrictions[from] = make(map[int64]bool)
 	}
+
+	graph.restrictions[from][to] = true
 
 	return nil
 }
