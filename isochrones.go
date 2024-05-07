95813cb6a4391f7bd8805ac306ab00ef2dbed36f
Magnus Hiie <magnus.hiie@starship.co>
2024-05-07 15:07:14 +0300

    doc: fix comment for Isochrones

--- isochrones.go
+++ isochrones.go
@@ -7,10 +7,8 @@ import (
 
 // Isochrones Returns set of vertices and corresponding distances restricted by maximum travel cost for source vertex
 // source - source vertex (user defined label)
-// maxCost - restriction on travel cost for breadth search
+// maxCost - restriction on travel cost
 // See ref. https://wiki.openstreetmap.org/wiki/Isochrone and https://en.wikipedia.org/wiki/Isochrone_map
-// Note: implemented breadth-first searching path algorithm does not guarantee shortest pathes to reachable vertices (until all edges have cost 1.0). See ref: https://en.wikipedia.org/wiki/Breadth-first_search
-// Note: result for estimated costs could be also inconsistent due nature of data structure
 func (graph *Graph) Isochrones(source int64, maxCost float64) (map[int64]float64, error) {
 	var ok bool
 	if source, ok = graph.mapping[source]; !ok {
