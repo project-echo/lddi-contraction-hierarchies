2d5d1ea2a491cc79a83993c45e37b88c288b3c23
Tanel Hiir <tanel.hiir@starship.co>
2024-10-15 20:24:00 +0300

    Use edge difference + deleted neighbours for priority
    It is unclear if current priority is intended or not.
    
    	shortcutCover := len(vertex.inIncidentEdges) * len(vertex.outIncidentEdges)
    	incidentEdgesNum := len(vertex.inIncidentEdges) + len(vertex.outIncidentEdges)
    	edgeDiff := shortcutCover - incidentEdgesNum
    	vertex.importance = edgeDiff + incidentEdgesNum + vertex.delNeighbors - vertex.bidirectedEdges()
    
    If we replace edgeDiff then we get:
    
    	vertex.importance = shortcutCover - incidentEdgesNum + incidentEdgesNum + vertex.delNeighbors - vertex.bidirectedEdges()
    
    which will simplify to:
    
    	vertex.importance = shortcutCover + vertex.delNeighbors - vertex.bidirectedEdges()

--- vertex.go
+++ vertex.go
@@ -55,24 +55,7 @@ func (vertex *Vertex) computeImportance() {
 	edgeDiff := shortcutCover - incidentEdgesNum
 	// [+] Spatial diversity heuristic: for each vertex maintain a count of the number of neighbors that have already been contracted [vertex.delNeighbors], and add this to the summary importance
 	// note: the more neighbours have already been contracted, the later this vertex will be contracted in further.
-	// [+] Bidirection edges heuristic: for each vertex check how many bidirected incident edges vertex has.
-	// note: the more bidirected incident edges == less important vertex is.
-	vertex.importance = edgeDiff + incidentEdgesNum + vertex.delNeighbors - vertex.bidirectedEdges()
-}
-
-// bidirectedEdges Number of bidirected edges
-func (vertex *Vertex) bidirectedEdges() int {
-	hash := make(map[int64]struct{}, len(vertex.inIncidentEdges))
-	for _, e := range vertex.inIncidentEdges {
-		hash[e.vertexID] = struct{}{}
-	}
-	ans := 0
-	for i := range vertex.outIncidentEdges {
-		if _, ok := hash[vertex.outIncidentEdges[i].vertexID]; ok {
-			ans++
-		}
-	}
-	return ans
+	vertex.importance = edgeDiff + vertex.delNeighbors
 }
 
 // Distance Information about contraction between source vertex and contraction vertex
